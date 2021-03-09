07cc762cb5476ba863cc700eabf0c0533466e2f9
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-09 15:10:34 -0500

    aa

--- base/global/git/linux/pUPDATE.sh
+++ base/global/git/linux/pUPDATE.sh
@@ -8,11 +8,10 @@ log_load pUPDATE.sh
 
 
 
-if [ -z $DIR_BNDL ]; then
-	log_invalid DIR_BNDL
+if [ -z $DIR_DTS ]; then
+	log_invalid DIR_DTS
 	exit 1
 fi
-log_var DIR_BNDL $DIR_BNDL
 
 source LOG.sh
 source pINIT.sh
@@ -25,35 +24,19 @@ pUPDATE() {
 
 	log_enter pUPDATE
 
-  log_var DIR_BNDL $DIR_BNDL
+  if [ -z $DIR_DTS ]; then
+    log_invalid DIR_DTS
+    exit 1
+  fi
+  log_var DIR_DTS $DIR_DTS
+
 	updateRepo $DIR_BNDL
 	log_var RES $J
 	if [ $? -ne 0 ]; then
 		return $?
 	fi
 
-  log_var DIR_GLBL $DIR_GLBL
-	updateRepo $DIR_GLBL
-	log_var RES $J
-	if [ $? -ne 0 ]; then
-		return $?
-	fi
-
-  log_var DIR_SYS $DIR_SYS
-	updateRepo $DIR_SYS
-	log_var RES $J
-	if [ $? -ne 0 ]; then
-		return $?
-	fi
-
-  log_var DIR_TMPL $DIR_TMPL
-	updateRepo $DIR_TMPL
-	log_var RES $J
-	if [ $? -ne 0 ]; then
-		return $?
-	fi
-
-	pINIT
+#	pINIT
 
 	log_exit pUPDATE
 
--- base/proj/init.sh
+++ base/proj/init.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2086
 
 
-# ECHO init.sh [LOAD]
+# echo init.sh [LOAD]
 
 
 DEF_URI_DTS_GIT=https://github.com/iTKunst/dts
@@ -12,44 +12,44 @@ init()
 {
 
   if [ ! -f settings.sh ]; then
-    ECHO settings.sh not found [FILE_ERR]
+    echo settings.sh not found [FILE_ERR]
     return 1
   fi
   source settings.sh
 
   if [ -z "$DIR_DTS" ]; then
-    ECHO DIR_DTS may be set in settings.sh. [INFO]
-    ECHO Setting to default value. [INFO]
+    echo DIR_DTS may be set in settings.sh. [INFO]
+    echo Setting to default value. [INFO]
     export DIR_DTS=$DEF_DIR_DTS
   fi
-  ECHO DIR_DTS is $DIR_DTS [VAR]
+  echo DIR_DTS is $DIR_DTS [VAR]
 
   if [ -d "$DIR_DTS" ]; then
-    ECHO Already initialized[INFO]
+    echo Already initialized[INFO]
     source ./bin/mSET_PATH.sh
-    ECHO Run pUPDATE [CMD]
+    echo Run pUPDATE [CMD]
     return 0
   fi
 
   if [ -z "$URI_DTS_GIT" ]; then
-    ECHO URI_DTS_GIT may be set in settings_uri.sh. [INFO]
-    ECHO Setting to default value [INFO].
+    echo URI_DTS_GIT may be set in settings_uri.sh. [INFO]
+    echo Setting to default value [INFO].
     export URI_DTS_GIT=$DEF_URI_DTS_GIT
   fi
-  ECHO URI_DTS_GIT is $URI_DTS_GIT [VAR]
+  echo URI_DTS_GIT is $URI_DTS_GIT [VAR]
 
   if [ -z $TMPL_NAME ]; then
-    ECHO TMPL_NAME [INVALID]
-    ECHO TMPL_NAME must be set in settings.sh. [INFO]
+    echo TMPL_NAME [INVALID]
+    echo TMPL_NAME must be set in settings.sh. [INFO]
 	exit
   fi
-  ECHO TMPL_NAME is $TMPL_NAME [VAR]
+  echo TMPL_NAME is $TMPL_NAME [VAR]
 
   export TMPL_FLDR="tmpl/"$TMPL_NAME"/*"
-  ECHO TMPL_FLDR is $TMPL_FLDR [VAR]
+  echo TMPL_FLDR is $TMPL_FLDR [VAR]
 
   export DIR_BNDL=$DIR_DTS/base/bundler
-  ECHO DIR_BNDL is $DIR_BNDL [VAR]
+  echo DIR_BNDL is $DIR_BNDL [VAR]
 
   mkdir -p $DIR_DTS
   cd $DIR_DTS
@@ -58,13 +58,13 @@ init()
   git remote add origin -f $URI_DTS_GIT
   git config core.sparsecheckout true
 
-  ECHO "base/*" >> .git/info/sparse-checkout
-  ECHO $TMPL_FLDR >> .git/info/sparse-checkout
+  echo "base/*" >> .git/info/sparse-checkout
+  echo $TMPL_FLDR >> .git/info/sparse-checkout
 
   git pull origin master
 
   if [ $?  -ne 0 ]; then
-      ECHO !!!!!ERROR!!!!! Unable to clone DTS base [CLONE_ERR]
+      echo !!!!!ERROR!!!!! Unable to clone DTS base [CLONE_ERR]
       return $?
   fi
 
@@ -72,11 +72,12 @@ init()
 
   if [ ! -d bin ]; then
     mkdir bin
-    ECHO create bin
+    echo create bin
   fi
 
   export DIR_BNDL=$DIR_DTS/base/bundler
 
+  source ./$DIR_BNDL/log/linux/LOG.sh
   source $DIR_BNDL/init.sh
   source bin/mSET_PATH.sh
   source bENV.sh
@@ -92,4 +93,4 @@ init()
 init
 
 
-# ECHO init.sh [UNLOAD]
+# echo init.sh [UNLOAD]
