f64f4bda9b3a218dba7ff5ce2057b2764fd4714a
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-10 18:38:20 -0500

    aa

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ source $DIR_BNDL/log/linux/LOG.sh
 
 log_enter /$DIR_DTS/init
 
-source .$DIR_DTS$DIR_BASE$INIT
-source .$DIR_DTS$DIR_TMPL$INIT
+source .$DIR_DTS$DIR_BASE$CMD_INIT
+source .$DIR_DTS$DIR_TMPL$CMD_INIT
 
 log_exit /$DIR_DTS/init
 
