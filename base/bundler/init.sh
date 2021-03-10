e45dc392451f6c2c6d13af6fa20c33e0f2a77e11
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-10 18:54:01 -0500

    aa

--- base/bundler/init.sh
+++ base/bundler/init.sh
@@ -4,10 +4,10 @@
 
 log_enter /$DIR_BNDL/init
 
-source ./$DIR_BNDL/env/init.sh
-source ./$DIR_BNDL/git/init.sh
-source ./$DIR_BNDL/log/init.sh
-source ./$DIR_BNDL/misc/init.sh
+#source ./$DIR_BNDL/env/init.sh
+#source ./$DIR_BNDL/git/init.sh
+#source ./$DIR_BNDL/log/init.sh
+#source ./$DIR_BNDL/misc/init.sh
 
 log_exit /$DIR_BNDL/init
 
