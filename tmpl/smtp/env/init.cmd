78981af34d6270a9db42b9f9d553f683135088c7
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-09 23:20:28 -0500

    aa

--- tmpl/smtp/env/init.cmd
+++ tmpl/smtp/env/init.cmd
@@ -3,14 +3,7 @@
 
 CALL LOG_ENTER \%DIR_TMPL%\env\init
 
-xcopy /q /y .\%DIR_TMPL%\env\win\*.cmd .\bin >NUL
+xcopy /q /y .\%DIR_TMPL%\env\win\tENV.cmd .\bin >NUL
 
-if not exist project (
-  mkdir project
-)
-
-if not exist project\pENV_MOD.cmd (
-  xcopy /q /y .\%DIR_TMPL%\env\win\pENV_MOD.sample.cmd .\project\pENV_MOD.cmd >NUL
-)
 
 CALL LOG_EXIT \%DIR_TMPL%\env\init
