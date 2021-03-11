b98a7113770bbbfcb5b5570183463713e8feafeb
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-10 20:32:44 -0500

    aa

--- base/bundler/env/init.sh
+++ base/bundler/env/init.sh
@@ -5,6 +5,10 @@ DIR=$DIR_DTS$DIR_BASE$DIR_BUNDLER$DIR_ENV
 
 log_enter $DIR$CMD_INIT
 
+log_var DIR $DIR
+log_var DIR $DIR_OS
+log_var DIR $DIR_BIN
+
 cp $DIR/$DIR_OS/*.sh $DIR_BIN
 
 log_exit $DIR$CMD_INIT
