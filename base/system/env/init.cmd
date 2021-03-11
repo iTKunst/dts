04170e9189ef281eed2d2d8c861f881154b8d7b8
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 12:05:23 -0500

    aa

--- base/system/env/init.cmd
+++ base/system/env/init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 
-call LOG_ENTER %SYS_DIR%%DIR_ENV%%CMD_INIT%
+call LOG_ENTER %DIR_SYS%%DIR_ENV%%CMD_INIT%
 
-xcopy /q /y %SYS_DIR%%DIR_ENV%%OS_DIR%\*.cmd .%BIN_DIR% >NUL
+xcopy /q /y %DIR_SYS%%DIR_ENV%%OS_DIR%\*.cmd .%BIN_DIR% >NUL
 
-call LOG_EXIT %SYS_DIR%%DIR_ENV%%CMD_INIT%
+call LOG_EXIT %DIR_SYS%%DIR_ENV%%CMD_INIT%
--- base/system/env/init.sh
+++ base/system/env/init.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2086
 
 
-log_enter $SYS_DIR$ENV_DIR$CMD_INIT
+log_enter $DIR_SYS$ENV_DIR$CMD_INIT
 
-cp $SYS_DIR$ENV_DIR$OS_DIR/*.sh .$DIR_BIN
+cp $DIR_SYS$ENV_DIR$OS_DIR/*.sh .$DIR_BIN
 
-log_exit $SYS_DIR$ENV_DIR$CMD_INIT
+log_exit $DIR_SYS$ENV_DIR$CMD_INIT
