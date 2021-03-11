fa879d4f751344e23dd6f7c7f4b3876e164bffed
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 11:32:31 -0500

    aa

--- tmpl/ng_cli/env/init.sh
+++ tmpl/ng_cli/env/init.sh
@@ -4,14 +4,14 @@
 
 log_enter /$DIR_TMPL/env/init
 
-cp $DIR_TMPL_CURR$ENV_DIR$DIR_OS/tENV.sh .$DIR_BIN
+cp $DIR_TMPL_CURR$ENV_DIR$OS_DIR/tENV.sh .$DIR_BIN
 
 if [ ! -d ./project ]; then
   mkdir project
 fi
 
 if [ ! -f ./project/pENV_MOD.sh ]; then
-  cp $DIR_TMPL_CURR$ENV_DIR$DIR_OS/pENV_MOD.sample.sh ./project/pENV_MOD.sh
+  cp $DIR_TMPL_CURR$ENV_DIR$OS_DIR/pENV_MOD.sample.sh ./project/pENV_MOD.sh
 fi
 
 log_exit /$DIR_TMPL/env/init
