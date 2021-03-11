70d0403e0319cd522ed044ceea9c5dec22170f1d
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-10 20:41:21 -0500

    aa

--- base/bundler/init.sh
+++ base/bundler/init.sh
@@ -5,11 +5,12 @@ export DIR=$DIR_DTS$DIR_BASE$DIR_BUNDLER
 
 log_enter $DIR$CMD_INIT
 
+log_var DIR $DIR
 
-source $DIR/env/init.sh
-source $DIR/git/init.sh
-source $DIR/log/init.sh
-source $DIR/misc/init.sh
+source $DIR/env$CMD_INIT
+source $DIR/git$CMD_INIT
+source $DIR/log$CMD_INIT
+source $DIR/misc$CMD_INIT
 
 log_exit $DIR$CMD_INIT
 
