c5eb74dd10ae208f5b5fbb0b65eea9e80a9b1df0
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 11:19:11 -0500

    aa

--- tmpl/smtp/init.sh
+++ tmpl/smtp/init.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2086
 
 
-log_enter $TMPL_DIR$CMD_INIT
+log_enter $TMPL_CURR_DIR$CMD_INIT
 
-source $TMPL_DIR$DIR_DCKR$CMD_INIT
-source $TMPL_DIR$DIR_ENV$CMD_INIT
+source $TMPL_CURR_DIR$DIR_DCKR$CMD_INIT
+source $TMPL_CURR_DIR$DIR_ENV$CMD_INIT
 
-log_exit $TMPL_DIR$CMD_INIT
+log_exit $TMPL_CURR_DIR$CMD_INIT
