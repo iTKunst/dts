7a546eb24e7eaf85dd5f3e43a9372977c171d163
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 07:53:14 -0500

    aa

--- tmpl/openapi_srvr/init.sh
+++ tmpl/openapi_srvr/init.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2086
 
 
-log_enter /$DIR_TMPL/init
+log_enter $TMPL_DIR$CMD_INIT
 
 source ./$DIR_TMPL/docker/init.sh
 source ./$DIR_TMPL/env/init.sh
 
-log_exit /$DIR_TMPL/init
+log_exit $TMPL_DIR$CMD_INIT
--- tmpl/swag_cg_cli/init.sh
+++ tmpl/swag_cg_cli/init.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC2086
 
 
-log_enter /$DIR_TMPL/init
+log_enter $TMPL_DIR$CMD_INIT
 
 source $TMPL_DIR$DIR_DCKR$CMD_INIT
 source ./$DIR_TMPL/env/init.sh
 
-log_exit /$DIR_TMPL/init
+log_exit $TMPL_DIR$CMD_INIT
