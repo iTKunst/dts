c75361a97cea8e38f5556acb35de72eee06305d9
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 17:40:39 -0500

    aa

--- base/global/init.sh
+++ base/global/init.sh
@@ -10,7 +10,7 @@ source $DIR_GLBL/docker_project$CMD_INIT
 source $DIR_GLBL/docker_system$CMD_INIT
 source $DIR_GLBL$ENV_DIR$CMD_INIT
 source $DIR_GLBL$GIT_DIR$CMD_INIT
-source $DIR_GLBL/kube$CMD_INITv
+source $DIR_GLBL/kube$CMD_INIT
 source $DIR_GLBL/openshift$CMD_INIT
 
 log_exit $DIR_GLBL$CMD_INIT
