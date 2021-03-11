77d692592982cd48ce5812cf8f4fbe9666efc12f
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 12:07:15 -0500

    aa

--- base/system/env/init.cmd
+++ base/system/env/init.cmd
@@ -1,8 +1,8 @@
 @echo off
 
 
-call LOG_ENTER %DIR_SYS%%DIR_ENV%%CMD_INIT%
+call LOG_ENTER %DIR_SYS%%ENV_DIR%%CMD_INIT%
 
-xcopy /q /y %DIR_SYS%%DIR_ENV%%OS_DIR%\*.cmd .%BIN_DIR% >NUL
+xcopy /q /y %DIR_SYS%%ENV_DIR%%OS_DIR%\*.cmd .%BIN_DIR% >NUL
 
-call LOG_EXIT %DIR_SYS%%DIR_ENV%%CMD_INIT%
+call LOG_EXIT %DIR_SYS%%ENV_DIR%%CMD_INIT%
