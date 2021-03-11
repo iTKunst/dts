e3c5e9d37e6d4d4ddbeb048272ab4d5a09b522d4
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-11 00:54:59 -0500

    aa

--- base/bundler/env/linux/bENV_NAMES.sh
+++ base/bundler/env/linux/bENV_NAMES.sh
@@ -5,10 +5,18 @@
 echo bENV_NAME [ENTER]
 
 export BASE=base
-export BUNDLER=bundler
-export GLOBAL=global
-export SYSTEM=system
-export TEMPL=tmpl
+export BNDL=bundler
+export DCKR=docker
+export ENV=env
+export EXT=sh
+export GIT=git
+export GLBL=global
+export LOG=log
+export MISC=misc
+export OS=linux
+export SYS=system
+export TMPL=tmpl
+
 
 echo bENV_NAME [EXIT]
 
