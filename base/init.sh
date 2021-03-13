2b7e77018e1a94b47489f4d56e38cce4c4b7102c
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-12 19:49:39 -0500

    aa

--- base/init.sh
+++ base/init.sh
@@ -8,8 +8,8 @@ log_enter $DIR_BASE$CMD_INIT
 source $DIR_BNDL$CMD_INIT
 source $DIR_GLBL$CMD_INIT
 
-cp $DIR_PROJ$CMD_INIT project
-cp $DIR_PROJ$CMD_UPDATE project
+cp $DIR_PROJ$CMD_INIT .
+cp $DIR_PROJ$CMD_UPDATE .
 
 
 log_exit $DIR_BASE$CMD_INIT
