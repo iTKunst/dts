d3edee6b6c360e7acbb8e737af3d7d5a112bcfa1
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-15 01:41:33 -0400

    aa

--- base/tmpl/env/init.cmd
+++ base/tmpl/env/init.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 
-CALL LOG_ENTER \%DIR_TMPL%%ENV_DIR%%CMD_INIT%
+CALL LOG_ENTER \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
 
 xcopy /q /y %DIR_TMPL%%ENV_DIR%%OS_DIR%\tENV.cmd %DIR_BIN% >NUL
 
@@ -13,4 +13,4 @@ if not exist project\pENV.cmd (
   xcopy /q /y %DIR_TMPL%%ENV_DIR%%OS_DIR%\pENV.sample.cmd .\project\pENV.cmd >NUL
 )
 
-CALL LOG_EXIT \%DIR_TMPL%%ENV_DIR%%CMD_INIT%
+CALL LOG_EXIT \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
