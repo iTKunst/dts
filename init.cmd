820b4f1f5419de5015308f2e55d59fd327227a84
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-15 01:34:58 -0400

    aa

--- init.cmd
+++ init.cmd
@@ -1,10 +1,10 @@
 @echo off
 
 
-REM echo dts\init.cmd [ENTER]
+echo dts\init.cmd [ENTER]
 
   REM TOKENS
-  SET "BCK_SLASH=\\"
+  SET "BCK_SLASH=\"
   SET "COLON=:"
   SET "DOT=."
   SET "FOR_SLASH=/"
@@ -17,7 +17,7 @@ REM echo dts\init.cmd [ENTER]
   SET "COM=com"
   SET "DTS=dts"
   SET "ENV=env"
-  SET "EXT=sh"
+  SET "EXT=cmd"
   SET "GIT=git"
   SET "GIT_HUB=github"
   SET "GLBL=glbl"
@@ -27,16 +27,16 @@ REM echo dts\init.cmd [ENTER]
   SET "ITK=iTKunst"
   SET "LOG=log"
   SET "MISC=misc"
-  SET "OS=linux"
+  SET "OS=win"
   SET "PROJ=proj"
   SET "SETTINGS=settings"
   SET "SYS=sys"
   SET "TMPL=tmpl"
 
   REM DIRS
-  SET "DIR_SLASH=%FOR_SLASH%"
+  SET "DIR_SLASH=%BCK_SLASH%"
 
-  SET "BASE_DIR=%DIR_SLASH%BASE%"
+  SET "BASE_DIR=%DIR_SLASH%%BASE%"
   SET "BIN_DIR=%BIN%"
   SET "BNDL_DIR=%DIR_SLASH%%BNDL%"
   SET "ENV_DIR=%DIR_SLASH%%ENV%"
@@ -54,48 +54,52 @@ REM echo dts\init.cmd [ENTER]
   SET "FILE_LOG=%SLASH'LOG'%%DOT%%EXT%"
   SET "FILE_SETTINGS=%SETTINGS%%DOT%%EXT%"
 
+  SET "DTS_DIR=dts"
+  ECHO DTS_DIR is %DTS_DIR% [INFO]
+
   SET "DIR_BASE=%DTS_DIR%%BASE_DIR%"
-  :: ECHO DIR_BASE is %DIR_BASE% [INFO]
+  ECHO DIR_BASE is %DIR_BASE% [INFO]
 
   SET "DIR_BIN=%BIN_DIR%"
-  :: ECHO DIR_BIN is %DIR_BIN% [INFO]
+  ECHO DIR_BIN is %DIR_BIN% [INFO]
 
   SET "DIR_BNDL=%DIR_BASE%%BNDL_DIR%"
-  :: ECHO DIR_BNDL is %DIR_BNDL% [INFO]
+  ECHO DIR_BNDL is %DIR_BNDL% [INFO]
 
   SET "DIR_ENV=%DIR_BNDL%%ENV_DIR%"
-  :: ECHO DIR_BNDL is %DIR_BNDL% [INFO]
+  ECHO DIR_BNDL is %DIR_BNDL% [INFO]
 
   SET "DIR_GLBL=%DIR_BNDL%%GLBL_DIR%"
-  :: ECHO DIR_GLBL is %DIR_GLBL% [INFO]
+  ECHO DIR_GLBL is %DIR_GLBL% [INFO]
 
   SET "DIR_PROJ=%DIR_BASE%%PROJ_DIR%"
-  :: ECHO DIR_PROJ is %DIR_PROJ% [INFO]
+  ECHO DIR_PROJ is %DIR_PROJ% [INFO]
 
   SET "DIR_SYS=%DIR_BASE%%SYS_DIR%"
-  :: ECHO DIR_SYS is %DIR_SYS% [INFO]
+  ECHO DIR_SYS is %DIR_SYS% [INFO]
 
   SET "DIR_TMPL=%DIR_BASE%%SYS_DIR%"
-  :: ECHO DIR_TMPL is %DIR_TMPL% [INFO]
+  ECHO DIR_TMPL is %DIR_TMPL% [INFO]
 
   SET "FILES=%OS_DIR%%DIR_SLASH%%STAR%%DOT%%EXT%"
-  :: ECHO FILES is %FILES% [INFO]
+  ECHO FILES is %FILES% [INFO]
 
   if [%TMPL_NAME%]==[] (
     ECHO TMPL_NAME [INVALID]
     ECHO TMPL_NAME must be set in settings.sh. [INFO]
     GOTO :EOF
   )
-  :: ECHO TMPL_NAME is %TMPL_NAME%
+  ECHO TMPL_NAME is %TMPL_NAME%
 
   SET "DIR_TMPL_CURR=%DIR_TMPL%%DIR_SLASH%%TMPL_NAME%"
-  :: ECHO DIR_TMPL_CURR is %DIR_TMPL_CURR% [INFO]
+  ECHO DIR_TMPL_CURR is %DIR_TMPL_CURR% [INFO]
 
-  cd %DIR_DTS%
-  git sparse-checkout set %DIR_BASE%%DIR_SLASH%%STAR% %DIR_BASE%%DIR_BNDL% %DIR_BASE%%DIR_GLBL% %DIR_BASE%%DIR_PROJ% %DIR_TMPL_CURR%
-  cd ..
+   if not exist %DIR_BIN% (
+     mkdir %DIR_BIN%
+     echo create %DIR_BIN%
+   )
 
-  CALL %DIR_ENV%%DIR_SLASH%%CMD_INIT%
+  CALL %DIR_ENV%%DIR_SLASH%%FILE_INIT%
   CALL %DIR_BIN%/bENV
   CALL %FILE_LOG%
   CALL LOG_LOAD %DIR_DTS%%CMD_INIT%
