0101910239c7f119ee08eb2c15087bf20ce48bcf
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 18:20:25 -0400

    aa

--- base/bndl/misc/win/mSET_PATH.cmd
+++ base/bndl/misc/win/mSET_PATH.cmd
@@ -1,18 +1,18 @@
 @echo off
 
 
-echo mSET_PATH [ENTER]
+rem echo mSET_PATH [ENTER]
 
 rem CALL %DIR_BIN%%DIR_SLASH%LOG_VAR PATH %PATH%
 rem echo PATH is %PATH%
 
 SET "FIND=%CD%%DIR_SLASH%%DIR_BIN"
 rem CALL %DIR_BIN%%DIR_SLASH%LOG_VAR FIND %FIND%
-echo  FIND is %FIND%
+rem echo  FIND is %FIND%
 
 ECHO "%PATH%" | findstr /C:"%FIND%">nul && (
   rem CALL %DIR_BIN%%DIR_SLASH%LOG_DUP PATH
-  echo PATH is already set
+  rem echo PATH is already set
   GOTO :END
 )
 
@@ -22,7 +22,7 @@ ECHO SET "PATH=%CD%%DIR_SLASH%%DIR_BIN%;%PATH%" >path.cmd
 
 :END
 
-echo mSET_PATH [EXIT]
+rem echo mSET_PATH [EXIT]
 
 EXIT /B %ERRORLEVEL%
 
