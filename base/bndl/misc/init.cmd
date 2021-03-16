3cda9db788f20673961346a93f9e962ccb087f7b
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 18:16:27 -0400

    aa

--- base/bndl/misc/init.cmd
+++ base/bndl/misc/init.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 
-echo  %DIR_BNDL%%DIR_SLASH%misc%DIR_SLASH%%FILE_INIT% [ENTER]
+rem echo  %DIR_BNDL%%DIR_SLASH%misc%DIR_SLASH%%FILE_INIT% [ENTER]
 
 SET SRC=%DIR_BNDL%%DIR_SLASH%misc%FILES%
-echo SRC is %SRC%
+rem echo SRC is %SRC%
 
 xcopy /q /y %SRC% %DIR_BIN% >NUL
 
-echo  %DIR_BNDL%%DIR_SLASH%misc%DIR_SLASH%%FILE_INIT% [EXIT]
+rem echo  %DIR_BNDL%%DIR_SLASH%misc%DIR_SLASH%%FILE_INIT% [EXIT]
