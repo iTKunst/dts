4fd769d743d4b81ab2ac4afd97350a18f8a843d7
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 18:17:25 -0400

    aa

--- base/bndl/git/init.cmd
+++ base/bndl/git/init.cmd
@@ -4,7 +4,7 @@
 rem echo  %DIR_BNDL%%DIR_SLASH%git%DIR_SLASH%%FILE_INIT% [ENTER]
 
 SET SRC=%DIR_BNDL%%DIR_SLASH%git%FILES%
-echo SRC is %SRC%
+rem echo SRC is %SRC%
 
 xcopy /q /y %SRC% %DIR_BIN% >NUL
 
--- base/bndl/log/init.cmd
+++ base/bndl/log/init.cmd
@@ -4,7 +4,7 @@
 rem echo  %DIR_BNDL%%DIR_SLASH%log%DIR_SLASH%%FILE_INIT% [ENTER]
 
 SET SRC=%DIR_BNDL%%DIR_SLASH%log%FILES%
-echo SRC is %SRC%
+rem echo SRC is %SRC%
 
 xcopy /q /y %SRC% %DIR_BIN% >NUL
 
