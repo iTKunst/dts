7330d62c9a85664453cb34ad26b064355d1de1b1
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 18:00:12 -0400

    aa

--- base/tmpl/env/init.cmd
+++ base/tmpl/env/init.cmd
@@ -2,17 +2,25 @@
 
 
 rem CALL %DIR_BIN%%DIR_SLASH%LOG_ENTER %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
-echo %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [ENTER]
+echo %DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [ENTER]
 
-xcopy /q /y %DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%\tENV.cmd %DIR_BIN% >NUL
+SET "SRC_TMPL=%DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%%DIR_SLASH%tENV.cmd"
+echo SRC_TMPL is %SRC_TMPL%
+
+xcopy /q /y %SRC_TMPL% %DIR_BIN% >NUL
 
 if not exist project (
+  echo making project directory
   mkdir project
 )
 
 if not exist project\pENV.cmd (
-  xcopy /q /y %DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%\pENV.sample.cmd project\pENV.cmd >NUL
+  echo copying pENV.cmd to project
+  SET "SRC_PROJ=%DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%%DIR_SLASH%pENV.sample.cmd"
+  echo SRC_PROJ is %SRC_PROJ%
+
+  xcopy /q /y %SRC_PROJ% project\pENV.cmd >NUL
 )
 
-CALL LOG_EXIT \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
-echo %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [EXIT]
+rem CALL LOG_EXIT \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
+echo %DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [EXIT]
