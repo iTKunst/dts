ddfb06080261d81204a432139368eb72691c9d0a
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 15:14:40 -0400

    aa

--- base/glbl/openshift/init.cmd
+++ base/glbl/openshift/init.cmd
@@ -2,7 +2,9 @@
 
 
 rem call %DIR_BIN%%DIR_SLASH%LOG_ENTER %DIR_GLBL%\openshift%DIR_SLASH%%FILE_INIT%
+echo %DIR_GLBL%%DIR_SLASH%os%DIR_SLASH%%FILE_INIT% [ENTER]
 
 xcopy /q /y %DIR_GLBL%\openshift%FILES% %DIR_BIN% >NUL
 
-call LOG_EXIT %DIR_GLBL%\openshift%DIR_SLASH%%FILE_INIT%
+rem call LOG_EXIT %DIR_GLBL%\openshift%DIR_SLASH%%FILE_INIT%
+echo %DIR_GLBL%%DIR_SLASH%os%DIR_SLASH%%FILE_INIT% [EXIT]
