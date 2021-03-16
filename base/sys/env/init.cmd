7bf5ccd9582ba832fc7bbcadca51042310d383eb
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 19:46:35 -0400

    aa

--- base/sys/env/init.cmd
+++ base/sys/env/init.cmd
@@ -7,7 +7,7 @@ echo .%DIR_SLASH%..%DIR_SLASH%system%ENV_DIR%%DIR_SLASH%%FILE_INIT% [ENTER]
   SET "SRC=..%DIR_SLASH%..%DIR_SLASH%system%ENV_DIR%%FILES%"
   ECHO SRC is %SRC%
 
-  xcopy /q /y SRC %DIR_BIN% >NUL
+  xcopy /q /y  %SRC% %DIR_BIN% >NUL
 
 rem call LOG_EXIT %DIR_SYS%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
 echo .%DIR_SLASH%..%DIR_SLASH%system%ENV_DIR%%DIR_SLASH%%FILE_INIT% [EXIT]
