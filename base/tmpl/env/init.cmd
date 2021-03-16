8a9ea1569ea6e0ddc0de8c68396f59017041c2fe
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-16 15:40:19 -0400

    aa

--- base/tmpl/env/init.cmd
+++ base/tmpl/env/init.cmd
@@ -1,16 +1,18 @@
 @echo off
 
 
-CALL %DIR_BIN%%DIR_SLASH%LOG_ENTER \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
+rem CALL %DIR_BIN%%DIR_SLASH%LOG_ENTER %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
+echo %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [ENTER]
 
-xcopy /q /y %DIR_TMPL%%ENV_DIR%%OS_DIR%\tENV.cmd %DIR_BIN% >NUL
+xcopy /q /y %DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%\tENV.cmd %DIR_BIN% >NUL
 
 if not exist project (
   mkdir project
 )
 
 if not exist project\pENV.cmd (
-  xcopy /q /y %DIR_TMPL%%ENV_DIR%%OS_DIR%\pENV.sample.cmd .\project\pENV.cmd >NUL
+  xcopy /q /y %DIR_TMPL_CURR%%ENV_DIR%%OS_DIR%\pENV.sample.cmd project\pENV.cmd >NUL
 )
 
 CALL LOG_EXIT \%DIR_TMPL%%ENV_DIR%%DIR_SLASH%%FILE_INIT%
+echo %DIR_TMPL_CURR%%ENV_DIR%%DIR_SLASH%%FILE_INIT%  [EXIT]
