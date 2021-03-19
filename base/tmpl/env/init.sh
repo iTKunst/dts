94b70f1035c69e3fdd6ff08ea870d0dd7df0d803
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-19 00:37:32 -0400

    aa

--- base/tmpl/env/init.sh
+++ base/tmpl/env/init.sh
@@ -6,5 +6,17 @@ log_enter $DIR_TMPL$ENV_DIR$DIR_SLASH$FILE_INIT
 
 cp $DIR_TMPL_CURR$ENV_DIR$OS_DIR/tENV.sh $DIR_BIN
 
+  if [ ! -d project ]; then
+    mkdir project
+   # echo create project
+  fi
+
+  if [ ! -d project/pENV.sh ]; then
+    rem echo copying pENV.sh to project
+    SRC_PROJ=$DIR_TMPL_CURR$ENV_DIR$OS_DIR$DIR_SLASH$ENV.sample.cmd
+    echo SRC_PROJ is $SRC_PROJ
+
+
+  fi
 
 log_exit $DIR_TMPL$ENV_DIR$DIR_SLASH$FILE_INIT
