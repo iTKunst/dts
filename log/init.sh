76e0922c76f30e80c9e5d158f43fa62e02d04a36
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-23 00:49:05 -0400

    aa

--- log/init.sh
+++ log/init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # shellcheck disable=SC2086
 
-LOG_DIR=$DIR_SLASH"log"
 
-log_enter $DIR_BNDL$LOG_DIR$FILE_INIT
+echo $DIR_LOG$FILE_INIT
 
-cp $DIR_BNDL$LOG_DIR$FILES $DIR_BIN
+cp $DIR_LOG$FILES $DIR_BIN
 
-log_exit $DIR_BNDL$LOG_DIR$FILE_INIT
+log_exit $DIR_LOG$FILE_INIT
