83f80d0229250ff3f2fdee306c991dce2f336a6d
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-23 01:16:18 -0400

    aa

--- env/init.sh
+++ env/init.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2086
 
 
-log_enter $DIR_GLBL$ENV_DIR$FILE_INIT
+log_enter $DIR_ENV$FILE_INIT
 
-cp $DIR_GLBL$ENV_DIR$FILES $DIR_BIN
+cp $DIR_ENV$FILES $DIR_BIN
 
-log_exit $DIR_GLBL$ENV_DIR$FILE_INIT
+log_exit $DIR_ENV$FILE_INIT
