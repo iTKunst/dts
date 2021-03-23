e4c2f46498762a1e2c84d277b3cb53ac0b532a11
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-23 01:11:54 -0400

    aa

--- boot/init.sh
+++ boot/init.sh
@@ -3,13 +3,13 @@
 
 
 
-echo $DIR_LOG$FILE_INIT [ENTER]
+echo $DIR_BOOT$FILE_INIT [ENTER]
 
-  cp $DIR_LOG$FILES $DIR_BIN
+  cp $DIR_BOOT$FILES $DIR_BIN
 
   export CMD_PATH=$DIR_BIN$FILE_PATH
   echo CMD_PATH is $CMD_PATH [VAR]
   source $CMD_PATH
-  source LOG.sh
+  source $FILE_LOG
 
 echo $DIR_LOG$FILE_INIT [EXIT]
