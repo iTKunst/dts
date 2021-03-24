d6e0e8f6e883365fc37c3ee11622da362ae44cbd
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-24 15:21:53 -0400

    aa

--- base/init.sh
+++ base/init.sh
@@ -10,7 +10,10 @@ log_enter $DIR_BASE$FILE_INIT
   SRC=$DIR_PROJ$FILE_INIT
   log_var SRC $SRC
 
-  cp $SRC .
+  TRG=$DIR_CURR
+  log_var TRG $TRG
+
+  cp $SRC $TRG
 
 log_exit $DIR_BASE$FILE_INIT
 
