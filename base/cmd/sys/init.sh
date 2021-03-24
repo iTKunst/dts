7679882f73cb59c9e2c6dc946e1687625a85af8f
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-24 02:06:30 -0400

    aa

--- base/cmd/sys/init.sh
+++ base/cmd/sys/init.sh
@@ -2,8 +2,8 @@
 # shellcheck disable=SC2086
 
 
-log_enter $DIR_CMD$DIR_SYS$FILE_INIT
+log_enter $DIR_CMD$SYS_DIR$FILE_INIT
 
-cp $DIR_CMD$DIR_SYS$FILES $DIR_BIN
+cp $DIR_CMD$SYS_DIR$FILES $DIR_BIN
 
-log_exit $DIR_CMD$DIR_SYS$FILE_INIT
+log_exit $DIR_CMD$SYS_DIR$FILE_INIT
