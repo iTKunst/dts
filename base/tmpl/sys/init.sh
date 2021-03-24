3362c2229b2a19bd8ca3e44641e9f2d293fdba3d
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-24 15:20:00 -0400

    aa

--- base/tmpl/sys/init.sh
+++ base/tmpl/sys/init.sh
@@ -2,14 +2,17 @@
 # shellcheck disable=SC2086
 
 
-log_enter $DIR_TMPL"sys"$FILE_INIT
+log_enter $DIR_BASE$TMPL_DIR$SYS_DIR$FILE_INIT
 
-SRC=../../system/maven
-TRG=system
+SRC=$DIR_SYS$MVN_DIR$FILE_SETTINGS
+log_var SRC $SRC
+
+TRG=$SYS
+log_var TRG $TRG
 
 mkdir -p $TRG
 
 cp $SRC $TRG
 
 
-log_exit $DIR_TMPL"sys"$FILE_INIT
+log_exit $DIR_BASE$TMPL_DIR$SYS_DIR$FILE_INIT
