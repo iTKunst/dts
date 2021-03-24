8c5f51cf84e34f339587e139bbb04311f00f3125
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-24 13:51:23 -0400

    aa

--- boot/env/init.sh
+++ boot/env/init.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2086
 
 
-log_enter $DIR_ENV$FILE_INIT
+log_enter $DIR_BOOT$ENV_DIR$FILE_INIT
 
-  cp $DIR_ENV$FILES $DIR_BIN
+  cp $DIR_BOOT$ENV_DIR$FILES $DIR_BIN
 
   source $FILE_ENV
 
-log_exit $DIR_ENV$FILE_INIT
+log_exit $DIR_BOOT$ENV_DIR$FILE_INIT
