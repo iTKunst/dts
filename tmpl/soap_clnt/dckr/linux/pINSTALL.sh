b8a33d68ffe12f14bd67bd03a6dfb4777db4e487
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-25 18:31:39 -0400

    aa

--- tmpl/soap_clnt/dckr/linux/pINSTALL.sh
+++ tmpl/soap_clnt/dckr/linux/pINSTALL.sh
@@ -11,14 +11,11 @@ log_enter pINSTALL
 
 source tENV.sh
 
-log_var SYS_DIR $SYS_DIR
-log_var PROJ_NAME $PROJ_NAME
-log_var SYS_NAME $SYS_NAME
 
-export TARGET=../$SYS_NAME$SPLIT$SYS_DIR/soap/$PROJ_NAME
-export SOURCE=./target/generated-sources/axis2
-
+export TARGET=$SYS_DIR$SOAP_DIR/$PROJ_NAME
 log_var TARGET $TARGET
+
+export SOURCE=./target/generated-sources/axis2
 log_var SOURCE $SOURCE
 
 mkdir -p $TARGET
