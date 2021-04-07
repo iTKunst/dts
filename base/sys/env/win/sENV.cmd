b0c8abc5a828a0a4a4a920fe069a12f229155f56
TAGHRdooleyp <pjldooley@gmail.com>
2021-04-06 20:46:42 -0400

    aa

--- base/sys/env/win/sENV.cmd
+++ base/sys/env/win/sENV.cmd
@@ -249,6 +249,17 @@ SET "WP_USER=root"
 
 SET "WP_TABLE_PREFIX=pre"
 
+REM WSDL
+
+SET "WSDL_DB_NAME="
+SET "WSDL_PASSWORD="
+SET "WSDL_USER="
+
+SET "WSDL_ART=cvms-scgv-ihs"
+SET "WSDL_FILE=APSShipService.wsdl"
+SET "WSDL_GRP=ca.gc.ccg-gcc"
+SET "WSDL_PKG=ca.gc.ccggcc.cvmsscgv.ihs"
+SET "WSDL_VER=1.0.1"
 
 rem call LOG_UNLOAD sENV
 echo sENV [UNLOAD]
