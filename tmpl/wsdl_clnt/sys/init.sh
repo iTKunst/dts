029b01d839e7b8aac5eb04bbb05eeb7a564ddb67
TAGHRdooleyp <pjldooley@gmail.com>
2021-04-06 20:43:53 -0400

    aa

--- tmpl/wsdl_clnt/sys/init.sh
+++ tmpl/wsdl_clnt/sys/init.sh
@@ -12,17 +12,22 @@ log_enter $DIR_TMPL_CURR$SYS_DIR$FILE_INIT
   log_var CMD_NEX $CMD_NEX
   source $CMD_NEX
 
-  SRC_CLNT=$DIR_SOAP_CLNT$DIR_SLASH$PROJ_NAME$DIR_SLASH$STAR
-  log_var SRC_CLNT $SRC_CLNT
+  CMD_WSDL=$DIR_SYS$SOAP_DIR$FILE_INIT
+  log_var CMD_WSDL $CMD_WSDL
+  source $CMD_WSDL
 
-  TRG_CLNT=$DIR_CODE_GEN_CLNT
-  log_var TRG_CLNT $TRG_CLNT
 
-  if [ ! -d $TRG_CLNT ]; then
-    mkdir -p $TRG_CLNT
-    echo create $TRG_CLNT
-  fi
+# SRC_CLNT=$DIR_SOAP_CLNT$DIR_SLASH$PROJ_NAME$DIR_SLASH$STAR
+# log_var SRC_CLNT $SRC_CLNT
 
-  cp -r $SRC_CLNT $TRG_CLNT
+# TRG_CLNT=$DIR_CODE_GEN_CLNT
+# log_var TRG_CLNT $TRG_CLNT
+
+# if [ ! -d $TRG_CLNT ]; then
+#   mkdir -p $TRG_CLNT
+#   echo create $TRG_CLNT
+# fi
+
+# cp -r $SRC_CLNT $TRG_CLNT
 
 log_exit $DIR_TMPL_CURR$SYS_DIR$FILE_INIT
